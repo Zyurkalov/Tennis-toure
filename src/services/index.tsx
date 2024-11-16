7bf80de59d13fb8dab39e1ab1b2277edf5081846
Zyurkalov <Zyurkalov@gmail.com>
2024-11-16 20:41:53 +0500

    refactor: added reduxjs/toolkit

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,11 +1,12 @@
-import {createStore, combineReducers} from 'redux'
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { todoReducer, menuReducer } from './reducers'
 
 const rootReducer = combineReducers({
     todos: todoReducer,
     menu: menuReducer,
 })
-export const store = createStore(rootReducer)
+export const store = configureStore({reducer: rootReducer})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
